a893c2f20246fb72ed672dbcc50aa714606cdcbe
liuleah <liuleah2005@gmail.com>
2018-11-06 17:19:31 -0800

    Updates

---
+++ precache-manifest.7fb02aa66239aa90c3cf28b8e8d0b97b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c78aaa2028cd6898f7cd",
+    "url": "/BreweryApp/static/css/main.3b14a410.chunk.css"
+  },
+  {
+    "revision": "c78aaa2028cd6898f7cd",
+    "url": "/BreweryApp/static/js/main.c78aaa20.chunk.js"
+  },
+  {
+    "revision": "68290131bddeaef51319",
+    "url": "/BreweryApp/static/js/1.68290131.chunk.js"
+  },
+  {
+    "revision": "9975ff8bff241ff32500",
+    "url": "/BreweryApp/static/js/runtime~main.9975ff8b.js"
+  },
+  {
+    "revision": "d41cf1ba7f934782c28f39daabdea069",
+    "url": "/BreweryApp/static/media/BreweryDB_top_logo.d41cf1ba.png"
+  },
+  {
+    "revision": "b52d75daa185b39fab345570b7ab2af2",
+    "url": "/BreweryApp/index.html"
+  }
+];
